3177758222878e16f0a0a371c375e446ef3221b7
Robert Fulcher <robert.fulcher@gmail.com>
2015-09-02 17:03:13 -0400

    Added code to dynamically pull data from all the form tables in the system.

--- application/modules/search/models/Search_model.php
+++ application/modules/search/models/Search_model.php
@@ -21,52 +21,63 @@ class Search_model extends CI_Model {
               
               if ($search_array['PaymentTransactionId'] != NULL)
               {
-                  $this->db->where('PaymentTransactionId', $search_array['PaymentTransactionId']);
+                  $this->db->where('payment_response.PaymentTransactionId', $search_array['PaymentTransactionId']);
               }
               
               if ($search_array['BegDate'] != NULL && $search_array['EndDate'] != NULL && $search_array['BegDate'] != "1969-12-31" && $search_array['EndDate'] != "1969-12-31")
               {
-                    $this->db->where('DATE(InsertDate) >=', $search_array['BegDate'])->where('DATE(InsertDate) <=', $search_array['EndDate']);
+                    $this->db->where('DATE(payment_response.InsertDate) >=', $search_array['BegDate'])->where('DATE(payment_response.InsertDate) <=', $search_array['EndDate']);
                     
               } else if ($search_array['BegDate'] != NULL)
               {
-                  $this->db->where('DATE(InsertDate) >=', $search_array['BegDate']);
+                  $this->db->where('DATE(payment_response.InsertDate) >=', $search_array['BegDate']);
                   
               } else if ($search_array['EndDate'] != NULL)
               {
-                  $this->db->where('DATE(InsertDate) <=', $search_array['EndDate']);
+                  $this->db->where('DATE(payment_response.InsertDate) <=', $search_array['EndDate']);
               }
               
               if ($search_array['PaymentSource'] != NULL)
               {
-                  $this->db->where('PaymentSource', $search_array['PaymentSource']);
+                  $this->db->where('payment_response.PaymentSource', $search_array['PaymentSource']);
               }
               
               if ($search_array['TransactionAmount'] != NULL)
               {
-                  $this->db->where('TransactionAmount', $search_array['TransactionAmount']);
+                  $this->db->where('payment_response.TransactionAmount', $search_array['TransactionAmount']);
               }              
               
               if ($search_array['AuthCode'] != NULL)
               {
-                  $this->db->where('AuthCode', $search_array['AuthCode']);
+                  $this->db->where('payment_response.AuthCode', $search_array['AuthCode']);
               }
               
               if ($search_array['OrderNumber'] != NULL)
               {
-                  $this->db->where('OrderNumber', $search_array['OrderNumber']);
+                  $this->db->where('payment_response.OrderNumber', $search_array['OrderNumber']);
               }
               
               if ($search_array['CVV2ResponseMessage'] != NULL)
               {
-                  $this->db->where('CVV2ResponseMessaaget', $search_array['CVV2ResponseMessage']);
+                  $this->db->where('payment_response.CVV2ResponseMessaaget', $search_array['CVV2ResponseMessage']);
               }
              
               if ($search_array['SerialNumber'] != NULL)
               {
-                  $this->db->where('SerialNumber', $search_array['SerialNumber']);
+                  $this->db->where('payment_response.SerialNumber', $search_array['SerialNumber']);
               }
-              
+
+              $form_list = $this->_get_forms();
+
+              foreach ($form_list->result() as $form) {
+                  $this->db->join($form->tablename, 'payment_response.PaymentTransactionId = '.$form->tablename.'.id and payment_response.PaymentSource = "'.$form->mnemonic.'" ', 'left');
+              }
+
+              // Sample of what the join should look like.
+              //$this->db->join('virtualterminal_submissions', 'payment_response.PaymentTransactionId = virtualterminal_submissions.id and payment_response.PaymentSource = "VT" ');
+              // Uncomment this to view the sql code in debug.
+              // $sql = $this->db->get_compiled_select();
+
               return $this->db->get();
               
           } else // search_array is empty
@@ -96,6 +107,11 @@ class Search_model extends CI_Model {
         $row = $q->row();
         
         return $row->amt;
+    }
+
+    private function _get_forms()
+    {
+        return $this->db->query('SELECT * FROM form_list');
     }
       
 }
