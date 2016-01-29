5da1fc067bd86bff961139ca9aea9d5b54e1b718
Matthew Kelch <matt@unicalabs.com>
2016-01-29 16:53:42 -0500

    Refactor donation form

--- application/modules/donation/controllers/Donation.php
+++ application/modules/donation/controllers/Donation.php
@@ -18,98 +18,50 @@ class Donation extends MX_Controller {
         $this->load->library('form_validation');
         $this->form_validation->CI =& $this;
 
+        // Load Required Modules
+        $this->load->module('payment');
         $this->load->module('configsys');
+        $this->load->module('nation_builder');
+        $this->load->module('email_sys');
 
-        // Your own constructor code
-        //$this->dx_auth->check_uri_permissions();
-
+        // Load Required Models
+        $this->load->model('Donation_model', 'Donation');
     }
 
+    /*
+     * Handles the default view
+     */
     public function index()
     {
-
-// Gather all the info for the view
-        $clientname = $this->configsys->get_config_value('Client_Name');
-        $clientaddress = $this->configsys->get_config_value('Client_Address');
-        $clientcity = $this->configsys->get_config_value('Client_City');
-        $clientstate = $this->configsys->get_config_value('Client_State');
-        $clientzip = $this->configsys->get_config_value('Client_Zip');
-        $clientphone = $this->configsys->get_config_value('Client_Phone');
-        $clientwebsite = $this->configsys->get_config_value('Client_Website');
-
-        $client_data = array(
-            'clientname' => $clientname,
-            'clientaddress' => $clientaddress,
-            'clientcity' => $clientcity,
-            'clientstate' => $clientstate,
-            'clientzip' => $clientzip,
-            'clientphone' => $clientphone,
-            'clientwebsite' => $clientwebsite
-        );
-
-        $data['client_data'] = $client_data;
-
-        $data['Donationform_Email_Required'] = $this->configsys->get_config_value('Donationform_Email_Required');
-
-        $data['Donationform_Logo'] = $this->configsys->get_config_value('Donationform_Logo');
-
-        $clientname = $this->configsys->get_config_value('Client_Name');
-
-        $view_vars = array(
-            'title' => '',
-            'heading' => $this->config->item('Client_Heading'),
-            'description' => $this->config->item('Client_Description'),
-            'company' => $clientname,
-            'logo' => $this->config->item('Client_Logo'),
-            'slogan' => '',
-            'author' => $this->config->item('Client_Author')
-        );
-        $data['page_data'] = $view_vars;
-
-        $this->load->view('donationform', $data);
+        // Display the guestform index
+        $this->load->view('donationform', $this->get_view_data());
     }
 
-
+    /*
+     * Handles submission of the donation form
+     */
     public function submit()
     {
-        $this->form_validation->set_rules('firstname', 'First Name', 'required|max_length[100]');
-        $this->form_validation->set_rules('middleinitial', 'Middle Initial', 'max_length[100]');
-        $this->form_validation->set_rules('lastname', 'Last Name', 'required|max_length[100]');
-        $this->form_validation->set_rules('streetaddress', 'Street Address', 'required|max_length[100]');
-        $this->form_validation->set_rules('city', 'City', 'required|max_length[100]');
-        $this->form_validation->set_rules('state', 'State', 'required|callback_check_default');
-        $this->form_validation->set_rules('zip', 'Zip Code', 'required|min_length[5]|max_length[5]');
-        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-        $this->form_validation->set_rules('employer', 'Employer', 'required|max_length[100]');
-        $this->form_validation->set_rules('occupation', 'Occupation', 'required|max_length[100]');
-        $this->form_validation->set_rules('creditcard', 'Credit Card', 'required|callback_check_creditcard');
-        $this->form_validation->set_rules('expirationmonth', 'Expiration Month', 'required');
-        $this->form_validation->set_rules('expirationyear', 'Expiration Year', 'required');
-        $this->form_validation->set_rules('cvv2', 'CVV2 Code', 'required|min_length[3]|max_length[4]');
-        $this->form_validation->set_rules('paymentamount', 'Payment Amount', 'required');
-		$this->form_validation->set_rules('otheramount', 'Other Amount', 'callback_check_otheramount');
-        $this->form_validation->set_rules('recurring', 'Recurring','trim');
-        $this->form_validation->set_rules('cardtype', 'Card Type','trim');
+        log_message('debug', 'Donation form submitted...');
+
+        // Call helper function to setup form validation
+        $this->setup_form_validation();
+
         $isrecurring = $this->input->post('recurring');
 
-
-
-        if ($this->form_validation->run($this) == FALSE)
+        if ($this->form_validation->run($this) == false)
         {
         	$this->index();
         }
-        else
-        {
-
-            // SAVE SUBMITTED FORM DATA
-            $this->load->model('Donation_model', 'Donation');
-			
+        else {
+            // Determine Payment Amount
 			if(strcasecmp($this->input->post('paymentamount'), 'other') == 0) {
 				$pamount = str_replace( ',', '', $this->input->post('otheramount') );
-			}
-			else {
+			} else {
 				$pamount = str_replace( ',', '', $this->input->post('paymentamount') );
 			}
+
+            // Construct array of submitted data
             $submitted_data = array(
                 'firstname' => $this->input->post('firstname'),
                 'middleinitial' => $this->input->post('middleinitial'),
@@ -130,7 +82,6 @@ class Donation extends MX_Controller {
                 'InsertDate' => date('Y-n-j H:i:s')
             );
 
-
             // Get inserted record id to use as transaction id.
             $transaction_id = $this->Donation->save($submitted_data);
 
@@ -142,60 +93,129 @@ class Donation extends MX_Controller {
             $submitted_data['cvv2'] = $this->input->post('cvv2');
             $submitted_data['PaymentSource'] = 'DF';
 
-
-            // PROCESS CREDIT CARD DATA
-            $this->load->module('payment');
+            // Process Credit Card  
             $result_data = $this->payment->process_payment($submitted_data);
+            $result_data_recurring = array();
 
-            $clientname = $this->configsys->get_config_value('Client_Name');
+            // Was the credit card processed successfully?
+            if($result_data['IsApproved'] == '1') {
+                // Trigger Events
+                Events::trigger('donation_payment_approved', $this->get_view_data($submitted_data, $result_data), 'string');
 
+                // Handle Recurring Dontation
+                if($this->input->post('recurring')[0] == 'recurring') {
+                    $recurring_data['recurring'] ='add_subscription';
+                    $recurring_data['plan_payments'] = '0';
+                    $recurring_data['plan_amount'] = $pamount;
+                    $recurring_data['month_frequency'] = '1';
+                    $recurring_data['day_of_month'] = date('d');
+                    $recurring_data['creditcard'] = $this->input->post('creditcard');
+                    $recurring_data['expirationmonth'] = $this->input->post('expirationmonth');
+                    $recurring_data['expirationyear'] = $this->input->post('expirationyear');
+                    $recurring_data['cardtype'] = $this->input->post('cardtype');
+                    $recurring_data['first_name'] = $this->input->post('firstname');
+                    $recurring_data['last_name'] = $this->input->post('lastname');
+                    $recurring_data['address1'] = $this->input->post('streetaddress');
+                    $recurring_data['city'] = $this->input->post('city');
+                    $recurring_data['state'] = $this->input->post('state');
+                    $recurring_data['zip'] = $this->input->post('zip');
+                    $recurring_data['email'] = $this->input->post('email');
 
-            // CHECK FOR RECURRING DONATION ADD TO THE SUBMISSION
-            if($isrecurring[0] == 'recurring' and $result_data['IsApproved'] == '1' ) {
-                $recurring_data['recurring'] ='add_subscription';
-                $recurring_data['plan_payments'] = '0';
-                $recurring_data['plan_amount'] = $pamount;
-                $recurring_data['month_frequency'] = '1';
-                $recurring_data['day_of_month'] = date('d');
-                $recurring_data['creditcard'] = $this->input->post('creditcard');
-                $recurring_data['expirationmonth'] = $this->input->post('expirationmonth');
-                $recurring_data['expirationyear'] = $this->input->post('expirationyear');
-                $recurring_data['cardtype'] = $this->input->post('cardtype');
-                $recurring_data['first_name'] = $this->input->post('firstname');
-                $recurring_data['last_name'] = $this->input->post('lastname');
-                $recurring_data['address1'] = $this->input->post('streetaddress');
-                $recurring_data['city'] = $this->input->post('city');
-                $recurring_data['state'] = $this->input->post('state');
-                $recurring_data['zip'] = $this->input->post('zip');
-                $recurring_data['email'] = $this->input->post('email');
+                    $this->load->module('recurring');
+                    $result_data_recurring = $this->recurring->addrecurring($recurring_data);
 
-                $this->load->module('recurring');
-                $result_data_recurring = $this->recurring->addrecurring($recurring_data);
-
-                $data['result_data_recurring'] = $result_data_recurring;
-
+                    $data['result_data_recurring'] = $result_data_recurring;
+                }
             }
 
+            // Load the donation form result view
+            $this->load->view('donationformresult', $this->get_view_data($submitted_data, $result_data, $result_data_recurring));
+        }
+    }
 
-            // Gather all the info for the view
-            $view_vars = array(
-                'title' => $this->config->item('Client_Title'),
-                'heading' => $this->config->item('Client_Heading'),
-                'description' => $this->config->item('Client_Description'),
-                'company' => $clientname,
-                'logo' => $this->config->item('Client_Logo'),
-                'slogan' => '',
-                'author' => $this->config->item('Client_Author')
-            );
-            $data['page_data'] = $view_vars;
-            $data['result_data'] = $result_data;
-            $data['is_recurring'] = $isrecurring[0];
-            $data['submitted_data'] = $submitted_data;
-            $this->load->view('donationformresult', $data);
+    /*
+     * Collects all required configuration data for the donation form views
+     */
+    private function get_view_data($submitted_data = array(), $result_data = array(), $recurring_data = array()) {
+        log_message('debug', 'Getting Donation view data...');
+
+        $data = array();
+
+        // Gather all the info for the view
+        $data['client_data'] = array(
+            'clientname' => $this->configsys->get_config_value('Client_Name'),
+            'clientaddress' => $this->configsys->get_config_value('Client_Address'),
+            'clientcity' => $this->configsys->get_config_value('Client_City'),
+            'clientstate' => $this->configsys->get_config_value('Client_State'),
+            'clientzip' => $this->configsys->get_config_value('Client_Zip'),
+            'clientphone' => $this->configsys->get_config_value('Client_Phone'),
+            'clientwebsite' => $this->configsys->get_config_value('Client_Website'),
+        );
+
+        $data['Donationform_Email_Required'] = $this->configsys->get_config_value('Donationform_Email_Required');
+
+        $data['Donationform_Logo'] = $this->configsys->get_config_value('Donationform_Logo');
+
+        $clientname = $this->configsys->get_config_value('Client_Name');
+
+        $view_vars = array(
+            'title' => '',
+            'heading' => $this->config->item('Client_Heading'),
+            'description' => $this->config->item('Client_Description'),
+            'company' => $clientname,
+            'logo' => $this->config->item('Client_Logo'),
+            'slogan' => '',
+            'author' => $this->config->item('Client_Author')
+        );
+        $data['page_data'] = $view_vars;
+        $data['result_data'] = $result_data;
+        $data['submitted_data'] = $submitted_data;
+
+        if(isset($recurring_data[0])) {
+            $data['is_recurring'] = $recurring_data[0];
+        } else {
+            $data['is_recurring'] = false;
         }
 
+        return $data;
+    }
 
+    /*
+     * Configures form validation for the guestform
+     */
+    private function setup_form_validation() {
+        log_message('debug', 'Setting up GuestForm form validation...');
 
+        $this->form_validation->set_rules('firstname', 'First Name', 'required|max_length[100]');
+        $this->form_validation->set_rules('middleinitial', 'Middle Initial', 'max_length[1]');
+        $this->form_validation->set_rules('lastname', 'Last Name', 'required|max_length[100]');
+        $this->form_validation->set_rules('streetaddress', 'Street Address', 'required|max_length[100]');
+        $this->form_validation->set_rules('city', 'City', 'required|max_length[100]');
+        $this->form_validation->set_rules('state', 'State', 'required|callback_check_default');
+        $this->form_validation->set_rules('zip', 'Zip Code', 'required|min_length[5]|max_length[5]');
+        $this->form_validation->set_rules('email', 'Email', 'valid_email');
+        $this->form_validation->set_rules('employer', 'Employer', 'required|max_length[100]');
+        $this->form_validation->set_rules('occupation', 'Occupation', 'required|max_length[100]');
+        $this->form_validation->set_rules('creditcard', 'Credit Card', 'required|callback_check_creditcard');
+        $this->form_validation->set_rules('expirationmonth', 'Expiration Month', 'required');
+        $this->form_validation->set_rules('expirationyear', 'Expiration Year', 'required');
+        $this->form_validation->set_rules('cvv2', 'CVV2 Code', 'required|min_length[3]|max_length[4]');
+        $this->form_validation->set_rules('paymentamount', 'Payment Amount', 'required');
+        $this->form_validation->set_rules('otheramount', 'Other Amount', 'callback_check_otheramount');
+        $this->form_validation->set_rules('recurring', 'Recurring','trim');
+        $this->form_validation->set_rules('cardtype', 'Card Type','trim');
+
+        // Handle notes field requirement
+        $Guestform_Notes_Required = $this->configsys->get_config_value('Guestform_Notes_Required');
+        if ($Guestform_Notes_Required == 'TRUE'){
+            $this->form_validation->set_rules('notes', 'Notes', 'required');
+        }
+
+        // Handle notes field requirement
+        $Guestform_Email_Required = $this->configsys->get_config_value('Guestform_Email_Required');
+        if ($Guestform_Email_Required == 'TRUE'){
+            $this->form_validation->set_rules('email', 'Email', 'required');
+        }
     }
 
     function check_default($post_string)
