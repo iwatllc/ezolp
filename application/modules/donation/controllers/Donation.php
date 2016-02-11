5051750d8cbed5d62439cf2e8eafa5ea59bcc5f3
Matthew Kelch <matt@unicalabs.com>
2016-02-10 21:59:40 -0500

    Fix typo in guestform and donationform email template.

--- application/modules/donation/controllers/Donation.php
+++ application/modules/donation/controllers/Donation.php
@@ -227,7 +227,7 @@ class Donation extends MX_Controller {
     private function get_email_body() {
         $message = '<!DOCTYPE html><html><body>';
         $message .= '<p>';
-        $message .= 'Than you for your payment';
+        $message .= 'Thank you for your payment';
         $message .= '<br>';
         $message .= 'Please keep this receipt for your records';
         $message .= '<br>';
