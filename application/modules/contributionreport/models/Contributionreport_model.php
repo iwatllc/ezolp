1ed61c4b801ada59902c2e9110f4c0e0c27fc33a
Matthew Kelch <matt@unicalabs.com>
2016-03-03 16:41:12 -0500

    Minor bugfix for Contributionreport_model.

--- application/modules/contributionreport/models/Contributionreport_model.php
+++ application/modules/contributionreport/models/Contributionreport_model.php
@@ -1,15 +1,15 @@
 <?php
 
-class Contributionreport_model extends CI_Model {
+use Report_model;
 
-    const STATUS_WAITING = 0;
-    const STATUS_IN_PROGRESS = 1;
-    const STATUS_COMPLETED = 2;
-    const STATUS_ERROR = 9;
+class Contributionreport_model extends Report_model {
     
     public function __construct()
     {
         parent::__construct();
+
+        // Configure the table name used for storing reports
+        $this->tableName = 'contribution_reports';
     }
      
     /**
@@ -47,20 +47,19 @@ class Contributionreport_model extends CI_Model {
 
     public function get_matching_list($filters) {
 
-        $startDate = DateTime::createFromFormat('m/d/Y', $filters['startDate'])->format("Y-m-d");
-        $endDate = DateTime::createFromFormat('m/d/Y', $filters['endDate'])->format("Y-m-d");
-
         $this->db->query('SET SQL_BIG_SELECTS=1');
         $this->db->select('donationform_submissions.*, contributors.* ');
         $this->db->from('donationform_submissions');
         $this->db->join('contributors', 'donationform_submissions.lastname = contributors.lastname AND donationform_submissions.firstname = contributors.firstname AND donationform_submissions.city = contributors.city');
         
         // Append where conditions when needed
-        if(!empty($startDate)) {
-            $this->db->where('donationform_submissions.InsertDate >=', $startDate);
+        if(!empty($filters['startDate'])) {
+            $startDate = DateTime::createFromFormat('m/d/Y', $filters['startDate'])->format("Y-m-d");
+            $this->db->where('contributors.transaction_date >=', $startDate);
         }
-        if(!empty($endDate)) {
-            $this->db->where('donationform_submissions.InsertDate <=', $endDate);
+        if(!empty($filters['endDate'])) {
+            $endDate = DateTime::createFromFormat('m/d/Y', $filters['endDate'])->format("Y-m-d");
+            $this->db->where('contributors.transaction_date <=', $endDate);
         }
         if(!empty($filters['zip'])) {
             $this->db->where('donationform_submissions.zip', $filters['zip']);
@@ -129,101 +128,6 @@ JOIN
         $query = $this -> db -> query($sql);
 
         return $query;
-    }
-
-    /*
-     * Persists the request for a new report in the database
-     */
-    public function create_new_report($input) {
-        $this->db->insert('contribution_reports', [
-            'input' => json_encode(['data' => $input]),
-            'creation_date' => date("Y-m-d H:i:s"),
-            'status_code' => Contributionreport_model::STATUS_WAITING,
-            ]);
-
-        return $this->db->insert_id();
-    }
-
-    /*
-     * Sets a report's status to STATUS_IN_PROGRESS
-     */
-    public function set_report_in_progress($id) {
-        $this->db->where('id', $id);
-        $this->db->update('contribution_reports', [
-                'status_code' => Contributionreport_model::STATUS_IN_PROGRESS,
-            ]);
-    }
-
-    /*
-     * Stores the result for a report and sets status to STATUS_COMPLETED
-     */
-    public function set_report_result($id, $result) {
-        $this->db->where('id', $id);
-        $this->db->update('contribution_reports', [
-                'results' => json_encode($result),
-                'status_code' => Contributionreport_model::STATUS_COMPLETED,
-            ]);
-    }
-
-    /*
-     * Retrieves a report
-     */
-    public function get_report($id) {
-        $this->db->from('contribution_reports');
-        $this->db->where('id', $id);
-        $query = $this->db->get();
-        $row = $query->row();
-
-        // Decode json
-        if(isset($row->input)) {
-            $row->input = json_decode($row->input)->data;
-        }
-        if(isset($row->results)) {
-            $row->results = json_decode($row->results);
-        }
-
-        return $row;
-    }
-
-    /*
-     * Retrieves all reports
-     */
-    public function get_all_reports() {
-        $this->db->select('id, input, creation_date, status_code');
-        $this->db->from('contribution_reports');
-        $this->db->order_by('id', 'DESC');
-        $query = $this->db->get();
-
-        $result = array();
-
-        foreach ($query->result() as $row)
-        {
-            // Decode json
-            if(isset($row->input)) {
-                $row->input = json_decode($row->input)->data;
-            }
-            // Status Names
-            switch ($row->status_code) {
-                case Contributionreport_model::STATUS_WAITING:
-                    $row->status_name = "Queued";
-                    break;
-                case Contributionreport_model::STATUS_IN_PROGRESS:
-                    $row->status_name = "In Progress";
-                    break;
-                case Contributionreport_model::STATUS_COMPLETED:
-                    $row->status_name = "Completed";
-                    break;
-                case Contributionreport_model::STATUS_ERROR:
-                    $row->status_name = "Error";
-                    break;
-                default:
-                    $row->status_name = "Unknown";
-            }
-
-            $result[] = $row;
-        }
-
-        return $result;
     }
 
 
