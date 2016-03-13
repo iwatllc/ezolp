bb48599af06bf9c6515e0b2e5b24ca79640f1279
Matthew Kelch <matt@unicalabs.com>
2016-03-13 19:56:47 -0400

    Update db seed file with prospect_reports and contribution_reports tables.

--- puphpet/dbseed/ezolp.sql
+++ puphpet/dbseed/ezolp.sql
@@ -15416,3 +15416,32 @@ ALTER TABLE `virtualterminal_submissions`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+DROP TABLE IF EXISTS `prospect_reports`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `prospect_reports` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `input` longtext NOT NULL,
+  `results` longtext NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `status_code` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `contribution_reports`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contribution_reports` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `input` longtext NOT NULL,
+  `results` longtext NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `status_code` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `id_2` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
