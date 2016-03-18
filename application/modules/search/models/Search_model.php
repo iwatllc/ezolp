9879d3eace573555c6b2471d3bfb274b6b0c1117
aaronfrazer <aarfrazer@gmail.com>
2016-03-17 22:13:22 -0400

    Before search implemented

--- application/modules/search/models/Search_model.php
+++ application/modules/search/models/Search_model.php
@@ -22,35 +22,35 @@ class Search_model extends CI_Model {
               if ($search_array['PaymentTransactionId'] != NULL) {
                   $this->db->where('payment_response.PaymentTransactionId', $search_array['PaymentTransactionId']);
               }
-              
-              if ($search_array['BegDate'] != NULL && $search_array['EndDate'] != NULL && $search_array['BegDate'] != "1969-12-31" && $search_array['EndDate'] != "1969-12-31") {
+
+              if ($search_array['BegDate'] != NULL && $search_array['EndDate'] != NULL) {
                   $this->db->where('DATE(payment_response.InsertDate) >=', $search_array['BegDate'])->where('DATE(payment_response.InsertDate) <=', $search_array['EndDate']);
               } else if ($search_array['BegDate'] != NULL) {
                   $this->db->where('DATE(payment_response.InsertDate) >=', $search_array['BegDate']);
               } else if ($search_array['EndDate'] != NULL) {
                   $this->db->where('DATE(payment_response.InsertDate) <=', $search_array['EndDate']);
               }
-              
+
               if ($search_array['PaymentSource'] != NULL) {
                   $this->db->where('payment_response.PaymentSource', $search_array['PaymentSource']);
               }
-              
+
               if ($search_array['TransactionAmount'] != NULL) {
                   $this->db->where('payment_response.TransactionAmount', $search_array['TransactionAmount']);
-              }              
-              
+              }
+
               if ($search_array['AuthCode'] != NULL) {
                   $this->db->where('payment_response.AuthCode', $search_array['AuthCode']);
               }
-              
+
               if ($search_array['OrderNumber'] != NULL) {
                   $this->db->where('payment_response.OrderNumber', $search_array['OrderNumber']);
               }
-              
+
               if ($search_array['CVV2ResponseCode'] != NULL) {
                   $this->db->where('payment_response.CVV2ResponseCode', $search_array['CVV2ResponseCode']);
               }
-             
+
               if ($search_array['SerialNumber'] != NULL) {
                   $this->db->where('payment_response.SerialNumber', $search_array['SerialNumber']);
 				  $this->db->or_where('payment_response.TransactionFileName', $search_array['SerialNumber']);
@@ -66,14 +66,15 @@ class Search_model extends CI_Model {
 //                  // $this->db->join($form->tablename, 'payment_response.PaymentTransactionId = '.$form->tablename.'.id and payment_response.PaymentSource = "'.$form->mnemonic.'" ', 'left');
 //              }
 
-              $this->db->join('form_submissions', 'payment_response.PaymentTransactionId = form_submissions.id and payment_response.PaymentSource = form_submissions.paymentsource ', 'left');
+//              $this->db->join('form_submissions', 'payment_response.PaymentTransactionId = form_submissions.id and payment_response.PaymentSource = form_submissions.paymentsource ', 'left');
 
               $this->db->join('transaction_status', 'payment_response.TransactionStatusId = transaction_status.id', 'inner');
 
               $this->db->join('transaction_type', 'payment_response.TransactionTypeId = transaction_type.id', 'inner');
 
-              $this->db->join('classifiedad_submissions', 'payment_response.TransactionTypeId = classifiedad_submissions.id AND payment_response.PaymentSource = "CA"', 'inner');
+//              $this->db->join('classifiedad_submissions', 'payment_response.TransactionTypeId = classifiedad_submissions.id AND payment_response.PaymentSource = "CA"', 'left');
 
+              $this->db->join('displayad_submissions', 'payment_response.TransactionTypeId = displayad_submissions.id AND payment_response.PaymentSource = "DA"', 'left');
 
               $this->db->order_by('payment_response.InsertDate', 'DESC');
 
